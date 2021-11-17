c8d57d5e274ca2323d0ec38d896c8a4664ecca00
John Felechner <jfelechner@comcast.net>
2021-11-16 21:24:19 -0600

    Adjustments

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 
-var clicks = 0;
-function increaseLikes() {
-    clicks += 1;
-        document.getElementById("number").innerHTML = clicks;
+var clicks1 = 0;
+function increaseLikesUser1() {
+    clicks1 += 1;
+    document.getElementById("number1").innerHTML = clicks1;
+}
+
+var clicks2 = 0;
+function increaseLikesUser2() {
+    clicks2 += 1;
+    document.getElementById("number2").innerHTML = clicks2;
+}
+
+var clicks3 = 0;
+function increaseLikesUser3() {
+    clicks3 += 1;
+    document.getElementById("number3").innerHTML = clicks3;
 }
